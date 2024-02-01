8f694466e380f2ebf4b85e55d69f872a9533be73
TheCoder120602 <143860679+TheCoder120602@users.noreply.github.com>
2024-01-31 18:47:51 -0600

    Add files via upload

---
+++ Account.h
@@ -0,0 +1,48 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class Account
+{
+	friend class AccountChecking;
+	friend class AccountSaving;
+    protected:
+    int* accID;
+    string cusName;
+    double balance;
+	int accountType;
+
+    public:
+    Account();
+    ~Account();
+    Account(const Account& origObj);
+    Account& operator = (const Account& objToCopy);
+    Account& operator +=(const Account& origObj)
+	{
+		this->balance += origObj.balance;
+		return *this;
+	};
+    virtual void deposit(double amt)
+	{
+		balance += amt;
+	};
+    virtual void withdraw(double amt)
+	{
+		balance -= amt;
+	};
+    void setName(string cusName);
+    void setBalance(double balance);
+    void setAccID(int id);
+    string GetName() const;
+    double GetBal() const;
+    int GetaccID() const;
+	void setAccountType(int acctType);
+	int getAccountType() const;
+    
+};
+
+#endif
+
